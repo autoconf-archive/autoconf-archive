5c040b86a86f3c35b4d8b7ae80b0dc66f3b7e884
Peter Simons <simons@cryp.to>
2009-07-24 23:37:39 +0200

    gnulib: deleted submodule
    The announce-gen script needs access to the gnulib repository in order
    to determine the library's version string. The default location, where
    gnulib is expected to be, is $srcdir/gnulib. Quoting from maint.mk:
    
      gnulib_dir ?= $(srcdir)/gnulib
      gnulib-version = $$(cd $(gnulib_dir) && git describe)
    
    At first, it looked like adding gnulib as a submodule to that location
    would be a good idea (especially, since it's needed to bootstrap, too).
    After some thought, however, it feels like overkill. Submodules are
    meant to track the state of the other repository in the current branch,
    but we don't really need that information. We just need some moderately
    recent version of gnulib-tool somewhere in $PATH; that's good enough.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,15 @@
 
 set -eu
 
-gnulib/gnulib-tool --m4-base build-aux --source-base build-aux --import git-version-gen gitlog-to-changelog gnupload maintainer-makefile announce-gen
+if [ -x "gnulib/gnulib-tool" ]; then
+  gnulibtool=gnulib/gnulib-tool
+else
+  gnulibtool=gnulib-tool
+fi
+$gnulibtool --m4-base build-aux --source-base build-aux --import git-version-gen gitlog-to-changelog gnupload maintainer-makefile announce-gen
+
 echo TODO >AUTHORS
+
 (cd m4 && ../build-aux/gitlog-to-changelog -- master >../ChangeLog)
+
 autoreconf --install -Wall
