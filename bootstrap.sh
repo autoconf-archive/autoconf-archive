624f192f8e4e5b97de3f24a932ac0b8776060ddf
Peter Simons <simons@cryp.to>
2009-07-31 20:54:08 +0200

    bootstrap.sh: run sed to disable sc_file_system in maint.mk
    This is not a particularly nice solution, but it does work for now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,8 @@ else
 fi
 $gnulibtool --m4-base build-aux --source-base build-aux --import git-version-gen gitlog-to-changelog gnupload maintainer-makefile announce-gen
 
+sed -i -e 's/^sc_file_system:/disabled_sc_file_system:/' maint.mk
+
 echo TODO >AUTHORS
 
 build-aux/gitlog-to-changelog -- master >ChangeLog
