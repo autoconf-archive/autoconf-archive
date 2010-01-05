4fb5007bc80d027f09ca4ba1c790be198b5703d1
Peter Simons <simons@cryp.to>
2010-01-05 14:52:22 +0100

    doc/autoconf-archive.texi: updated after recent changes

--- fix-website.sh
+++ fix-website.sh
@@ -11,16 +11,7 @@ mkdir -p "$destdir"
 
 for n in *.html; do
   echo fixing "$n"
-  case "$n" in
-    index.html)
-      m="autoconf-archive.html"
-      ;;
-    *)
-      m="${n//_005f/_}"
-      ;;
-  esac
-  sed <"$n" >"$destdir/$m" \
+  sed <"$n" >"$destdir/${n//_005f/_}" \
     -e 's|_005f|_|g' \
-    -e 's|href="index.html|href="autoconf-archive.html|g' \
-    -e 's|href="../index.html|href="index.html|g'
+    -e 's|href="../index.html#dir">(dir)</a>|href="http://savannah.nongnu.org/projects/autoconf-archive/">Home Page at Savannah</a>|g'
 done
