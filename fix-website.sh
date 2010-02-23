d2a28302a8492de0a100e4ea57c6b40d5c3daa06
Peter Simons <simons@cryp.to>
2010-02-23 21:55:09 +0100

    fix-website.sh: run generated HTML through tidy to ensure it's clean

--- fix-website.sh
+++ fix-website.sh
@@ -11,7 +11,12 @@ mkdir -p "$destdir"
 
 for n in *.html; do
   echo fixing "$n"
-  sed <"$n" >"$destdir/${n//_005f/_}" \
+  out="${n//_005f/_}"
+  name="${out%%.html}"
+  sed <"$n" \
+    -e 's|<html lang="en">|<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN" "http://www.w3.org/TR/html4/strict.dtd"><html lang="en">|' \
+    -e "s|<a name=\"${name}\"></a>||" \
     -e 's|_005f|_|g' \
-    -e 's|href="../index.html#dir">(dir)</a>|href="http://savannah.nongnu.org/projects/autoconf-archive/">Home Page at Savannah</a>|g'
+    -e 's|href="../index.html#dir">(dir)</a>|href="http://savannah.nongnu.org/projects/autoconf-archive/">Home Page at Savannah</a>|g' \
+  | tidy >"$destdir/${out}" -q --indent yes --indent-spaces 1 -wrap 80 --tidy-mark no --hide-comments yes
 done
