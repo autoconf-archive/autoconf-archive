d5b2a88b2e212d447ac8e783bbbdde70eb079adc
Peter Simons <simons@cryp.to>
2010-06-04 14:07:53 +0200

    macro.py: dropped computation of the obsolete m4header variable
    It's not used anywhere anymore.

--- macro.py
+++ macro.py
@@ -73,11 +73,6 @@ class Macro:
     header = map(lambda l: l[2:], header.split('\n'))
     # set defaults
     self.authors = []
-    url = "http://www.nongnu.org/autoconf-archive/%s" % (self.name + ".html")
-    lineLen = max(75,len(url) + 2)
-    separator = '=' * lineLen
-    padding = ' ' * ((lineLen - len(url)) / 2)
-    self.m4header = "# %s\n"*3 % (separator, padding + url, separator)
     # parse each section in the remaining list
     for (key, body) in splitSections(header):
       # drop empty lines at beginning and end of body
