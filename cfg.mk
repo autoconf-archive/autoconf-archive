54f5058fcc13fd340f0bfdfd295d926da994faef
Peter Simons <simons@cryp.to>
2010-08-19 13:10:20 +0200

    post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -3,7 +3,7 @@
 # settings required by the maintainer-makefile module
 
 gnu_rel_host		:= ftp.gnu.org
-old_NEWS_hash		:= b489dd94913f35139866ccfcc9182b17
+old_NEWS_hash		:= b158e8f325bff81e2e93bf9dbf912eaa
 gpg_key_ID		:= 99089D72
 today			:= $(date "+%Y-%m-%d")
 TAR_OPTIONS		+= --mtime=$(today)
