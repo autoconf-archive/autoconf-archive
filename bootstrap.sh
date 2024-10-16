5c30529cc9e19c2fbc02fe611169bd02f23b4e38
Peter Simons <simons@cryp.to>
2024-10-16 11:44:28 +0200

    gnulib: disable the sc_unsigned_int check for our releases

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,6 +32,7 @@ sed -e 's/^sc_file_system:/disabled_sc_file_system:/' \
     -e 's/^sc_prohibit_doubled_word:/disabled_sc_prohibit_doubled_word:/' \
     -e 's/^sc_prohibit_atoi_atof:/disabled_sc_prohibit_atoi_atof:/' \
     -e 's/^sc_unportable_grep_q:/disabled_sc_unportable_grep_q:/' \
+    -e 's/^sc_unsigned_int:/disabled_sc_unsigned_int:/' \
   maint.mk > maint.mk.new
 mv maint.mk.new maint.mk
 
