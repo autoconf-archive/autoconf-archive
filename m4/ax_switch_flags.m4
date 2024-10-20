autoconf-archive/autoconf-archive

commit 2adff78e224c908fd58df91852c8301c25777a8f

ax_switch_flags.m4: validate macro arguments at autoconf compile time
This is a nicer experience for detecting outright misuse of macros than
successfully creating a configure script and then having it fail.

m4/ax_switch_flags.m4
@@ -36,12 +36,12 @@
 #   and this notice are preserved. This file is offered as-is, without any
 #   warranty.
 
-#serial 4
+#serial 5
 
 AC_DEFUN([AX_SWITCH_FLAGS], [
+  m4_if($1, [], m4_fatal([$0: namespace is empty]))
   AC_REQUIRE(AX_SAVE_FLAGS)
   AC_REQUIRE(AX_RESTORE_FLAGS)
-  AS_IF([test "X$1" = "X"], AC_MSG_ERROR(newnamespace is empty)]
   AX_SAVE_FLAGS($1[])
   AX_RESTORE_FLAGS($2[])
 ])

commit 3a23daa3da0eb1a256fda631867e19345d5f6e3a

ax_switch_flags.m4: properly quote m4_fatal
It needs to only run as an argument of m4_if, not all the time.

Fixes: 2adff78e224c908fd58df91852c8301c25777a8f

m4/ax_switch_flags.m4
@@ -36,10 +36,10 @@
 #   and this notice are preserved. This file is offered as-is, without any
 #   warranty.
 
-#serial 5
+#serial 6
 
 AC_DEFUN([AX_SWITCH_FLAGS], [
-  m4_if($1, [], m4_fatal([$0: namespace is empty]))
+  m4_if($1, [], [m4_fatal([$0: namespace is empty])])
   AC_REQUIRE(AX_SAVE_FLAGS)
   AC_REQUIRE(AX_RESTORE_FLAGS)
   AX_SAVE_FLAGS($1[])